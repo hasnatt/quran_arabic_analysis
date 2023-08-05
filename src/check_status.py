6725d2c26df55d2c967f345fee697202972dd521
Hasnat <hasnat@Hasnats-MacBook-Pro.local>
2023-08-06 00:26:44 +0100

    update check status

--- src/check_status.py
+++ src/check_status.py
@@ -1,16 +1,17 @@
+"""Module to check the ending of arabic words"""
 from tashkeel import Tashkeels
 
-def is_raf(word):
-    pass
 
-def is_nasb(word):
-    pass
+# def is_raf(word):
+#     # if list(word)[-1] == Tashkeels.KASRA.value:
+#     #     return True
+#     # return False
+#     pass
+
+# def is_nasb(word):
+#     pass
+
 
 def is_jarr(word):
-    if list(word)[-1] == Tashkeels.KASRA.value:
-        return True
-    return False
-
-
-
-
+    """check if arabic word ends with kasra"""
+    return list(word)[-1] == Tashkeels.KASRA.value
